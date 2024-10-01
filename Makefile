68200c8859a3a0a7f648ab6e0ddd843c2a41c2f1
Pablo Gonzalez Gonzalez <pablogon@c3r3s5.42malaga.com>
2024-10-01 20:10:59 +0200

    BaseMinishell

--- Makefile
+++ Makefile
@@ -6,7 +6,44 @@
 #    By: pablogon <pablogon@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/01 19:11:08 by pablogon          #+#    #+#              #
-#    Updated: 2024/10/01 19:11:09 by pablogon         ###   ########.fr        #
+#    Updated: 2024/10/01 19:30:14 by pablogon         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME:= minishell
+
+CFLAGS:= -g -Wall -Werror -Wextra \
+		#-g -fsanitize=address,undefined \
+		#-Wunreachable-code -Ofast \
+
+# Libft files and directories
+LIBFT_DIR = ./Libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+HEADERS := -I$(LIBFT)
+
+SRCS	:= 
+
+OBJS	:= ${SRCS:.c=.o}
+
+all: libft $(NAME)
+libft:
+	@make -C $(LIBFT_DIR)
+
+%.o: %.c
+	@$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS)
+$(NAME): $(OBJS)
+	@$(CC) $(CFLAGS) $(OBJS) $(LIBFT) $(HEADERS) $(LINK) -o $(NAME)
+	@echo "Compiling minishell..."
+
+clean:
+	@rm -rf $(OBJS)
+	@rm -rf $(LIBFT_DIR)/*.o
+
+fclean: clean
+	@rm -rf $(NAME)
+	@rm -rf $(LIBFT)
+
+re: fclean all
+
+.PHONY: all, clean, fclean, re, libft
---
+++ include/minishell.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pablogon <pablogon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 19:22:17 by pablogon          #+#    #+#             */
+/*   Updated: 2024/10/01 20:01:21 by pablogon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h> /*write,open,read,close,access,unlink,getcwd,chdir, isatty, ttyname, ttyslot, ioctl, tcsetattr, tcgetattr */
+
+# include <stdio.h> //printf
+
+# include <stdlib.h> //malloc, free, getenv
+
+# include <stdbool.h> //true, false
+
+# include <fcntl.h>  //open(), O_RDONLY, O_WRONLY...etc
+
+# include <limits.h> // INT_MAX, CHAR_MAX ...etc
+
+# include <sys/types.h> //fork()
+
+# include <sys/wait.h> //wait(), waitpid(), kill()
+
+# include <sys/stat.h> //stat,lstat, fstat
+
+# include <signal.h> //signal, sigaction, kill
+
+# include <dirent.h> //opendir, readdir, closedir
+
+# include <term.h> //tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+
+# include <readline/readline.h> /* readline, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay, add_history */
+
+# include "../Libft/libft.h"
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
