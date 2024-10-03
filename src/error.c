4f08524685532b90ee38e901a253079d131cc8ce
pablooglez <pablogon@student.42malaga.com>
2024-10-03 22:00:57 +0200

    BaseMinishell

--- src/error.c
+++ src/error.c
@@ -6,23 +6,26 @@
 /*   By: pablogon <pablogon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 19:55:18 by pablogon          #+#    #+#             */
-/*   Updated: 2024/10/03 21:31:16 by pablogon         ###   ########.fr       */
+/*   Updated: 2024/10/03 21:58:19 by pablogon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_error(t_minishell *shell, char *msg, int exit_code)
+void fatal(int code, char *value) 
 {
-	write (2, msg, ft_strlen(msg));
-	write (2, "\n", 1);
+	if (code == MEMORY)
+		printf("MEMORIA\n");
+	if (code == CMD_NOT_FOUND)
+		printf("CMD %s\n", value);
+}
 
-	shell->exit_status = exit_code;		//Actualizar estado de salida del shell en el código erroneo
+void	ft_error(t_minishell *shell, int code, char * value)
+{
+	fatal(code, value);
 
-	if (exit_code != 0)					//Si el error es crítico, limpiar recursos y salir
-	{
-		cleanup_minishell(shell);		//Limpiar recursos si es necesario
-		exit(exit_code);				//Terminar el programa
-	}
+	if (value) free(value);
+	//cleanup_minishell(shell);		//Limpiar recursos si es necesario
+	exit(shell->exit_status);				//Terminar el programa
 
 }
