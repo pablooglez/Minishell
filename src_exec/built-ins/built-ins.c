3996eb8e979d5b33df1bf1da9d2c5b34fc188e8e
pablogon <pabloglez@MacBook-Air-de-Pablo.local>
2024-10-15 14:17:53 +0200

    Execution

--- src_exec/built-ins/built-ins.c
+++ src_exec/built-ins/built-ins.c
@@ -6,7 +6,7 @@
 /*   By: pabloglez <pabloglez@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 19:15:09 by pabloglez         #+#    #+#             */
-/*   Updated: 2024/10/14 20:01:21 by pabloglez        ###   ########.fr       */
+/*   Updated: 2024/10/14 20:57:05 by pabloglez        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	handle_builtin(t_cmd *cmd, t_minishell *shell)
 	}
 	else if (ft_strcmp(cmd->arguments[0], "pwd", 4) == 0)			//Comprobamos si el comando es "pwd"
 	{
-		ft_pwd(shell);								//Llama a la función personalizada para "pwd"
+		ft_pwd(shell);												//Llama a la función personalizada para "pwd"
 		return (1);													//Retorna 1 para indicar que es un comando builtin
 	}
 	else if(ft_strncmp(cmd->arguments[0], "export", 7) == 0)		//Comprobamos si el comando es "export"
@@ -41,7 +41,7 @@ int	handle_builtin(t_cmd *cmd, t_minishell *shell)
 	}
 	else if (ft_strncmp(cmd->arguments[0], "env", 4) == 0)			//Comprobamos si el comando es "env"
 	{
-		ft_env(shell);								//Llama a la función personalizada para "env"
+		ft_env(shell);												//Llama a la función personalizada para "env"
 		return (1);													//Retorna 1 para indicar que es un comando builtin
 	}
 	else if (ft_strncmp(cmd->arguments[0], "exit", 5) == 0)			//Comprobamos si el comando es "exit"
