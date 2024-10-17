633ce65fbd597bb290bc0ae399b318ead89809a6
pablogon <pabloglez@MacBook-Air-de-Pablo.local>
2024-10-17 20:45:25 +0200

    Modificacion03

--- src_exec/built-ins/export.c
+++ src_exec/built-ins/export.c
@@ -6,8 +6,26 @@
 /*   By: pabloglez <pabloglez@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 20:00:11 by pabloglez         #+#    #+#             */
-/*   Updated: 2024/10/14 20:00:16 by pabloglez        ###   ########.fr       */
+/*   Updated: 2024/10/17 20:44:40 by pabloglez        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	ft_export(t_minishell *shell, char **arg)
+{
+	int	i;
+
+	i = 1;
+	
+	if (!arg[1])
+	{
+		print_env_vars(shell->env_vars);
+		return;
+	}
+
+	while (arg[i])
+	{
+		char *equal_sign
+	}
+}
