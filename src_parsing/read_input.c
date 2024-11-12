4580fea3c5efa6a6d4988f6324cdedff00ac5e57
pablooglez <pablooglez97@gmail.com>
2024-11-12 19:01:36 +0100

    Modi

--- src_parsing/read_input.c
+++ src_parsing/read_input.c
@@ -6,7 +6,7 @@
 /*   By: pabloglez <pabloglez@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 19:22:39 by albelope          #+#    #+#             */
-/*   Updated: 2024/11/05 13:43:41 by pabloglez        ###   ########.fr       */
+/*   Updated: 2024/11/06 17:38:22 by pabloglez        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 char *read_input(void)
 {
     char	*input;
-    char	cwd[1024];                                                  // Buffer para almacenar la ruta actual
+    //char	cwd[1024];                                                  // Buffer para almacenar la ruta actual
 
-    if (getcwd(cwd, sizeof(cwd)) != NULL)                               // Obtener el directorio actual
+    /*if (getcwd(cwd, sizeof(cwd)) != NULL)                               // Obtener el directorio actual
         printf("\033[1;32m%s\033[0m ", cwd);                            // Imprimir el directorio actual en color verde
     else
-        perror("getcwd");                                               // Manejar errores de getcwd si falla
+        perror("getcwd");*/                                               // Manejar errores de getcwd si falla
     input = readline("\033[1;36mMinishell ➜ \033[0m");                  // Mostrar prompt con color y leer la entrada del usuario
     if (!input)                                                         // Detectar EOF (Ctrl+D)
         return (NULL);                                                  // Retornar NULL para indicar EOF
